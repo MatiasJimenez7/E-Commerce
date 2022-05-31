6928d9d7be6d5d422281c575cc8dd316f32c40f9
MatiasJimenez7 <jimenezmatias007@gmail.com>
2022-05-31 01:19:24 -0300

    Desafio NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import "./NavBar.css"
+
+
+function NavBar () {
+    return (
+        <nav className="navbar-mayor">
+            <a href="#">Informacion</a>
+            <a href="#">Clientes</a>
+            <a href="#">Nosotros</a>
+        </nav>
+
+    )
+}
+
+export default NavBar;
